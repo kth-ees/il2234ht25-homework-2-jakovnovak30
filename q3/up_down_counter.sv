69fd60dfc1f0aa2cfd916b27a49298162e4f9bbf
jakovnovak30 <jakovnovak30@gmail.com>
2025-09-17 21:14:08 +0200

    q3 done

--- q3/up_down_counter.sv
+++ q3/up_down_counter.sv
@@ -1,4 +1,4 @@
-module up_down_counter #(parameter N = 4)
+module up_down_counter #(parameter unsigned N = 4)
                        (input  logic clk,
                         input  logic rst_n,
                         input  logic up_down,
@@ -6,7 +6,39 @@ module up_down_counter #(parameter N = 4)
                         input  logic [N-1:0] input_load,
                         output logic [N-1:0] count_out,
                         output logic carry_out);
-  
-  // complete here
+
+logic [N-1:0] state, carry;
+
+// sequential logic
+always_ff @(posedge clk or negedge rst_n) begin
+  // reset
+  if (!rst_n) begin
+    state <= '0;
+    carry <= '0;
+  end
+  else begin
+    // set
+    if (load) begin
+      state <= input_load;
+      carry <= '0;
+    end
+    // up
+    else if(up_down) begin
+      if (count_out == '1) carry <= 1;
+      else carry <= '0;
+      state <= state + 1;
+    end
+    // down
+    else begin
+      state <= state - 1;
+      carry <= 0;
+    end
+  end
+end
+
+// combinational logic
+assign count_out = state;
+assign carry_out = carry;
 
 endmodule
+
--- q3/up_down_counter_tb.sv
+++ q3/up_down_counter_tb.sv
@@ -1,5 +1,34 @@
 module up_down_counter_tb;
 
-// complete here
+logic clk = 0, rst_n = 1, up_down = 1, load = 0, carry_out;
+logic [3:0] input_load, count_out;
+
+up_down_counter UUT(
+   clk, rst_n, up_down, load,
+   input_load, count_out,
+   carry_out
+  );
+
+always begin
+  #5 clk = 1;
+  #5 clk = 0;
+end
+
+initial begin
+  rst_n = 0;
+  #10;
+  rst_n = 1;
+  load = 1;
+  input_load = 'b1101;
+  #10;
+  load = 0;
+  up_down = 1;
+  #30;
+  up_down = 0;
+  #30;
+
+  $stop;
+end
 
 endmodule
+
