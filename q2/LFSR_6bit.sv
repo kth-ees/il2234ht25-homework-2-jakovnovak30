0abf7047d7daad69bdd0d65899eb43df59c401b4
jakovnovak30 <jakovnovak30@gmail.com>
2025-09-17 20:59:43 +0200

    q2 done

--- q2/LFSR_6bit.sv
+++ q2/LFSR_6bit.sv
@@ -4,7 +4,35 @@ module LFSR_6bit (
   input  logic [5:0] parallel_in,
   output logic [5:0] parallel_out
 );
-  // …
-  // Add your description here
-  // …
+
+logic [5:0] bits;
+logic [5:0] next_bits, xor_results;
+
+// sequential part
+always_ff @(posedge clk or negedge rst_n) begin : seq_logic
+  if (!rst_n) begin
+    next_bits <= '0;
+  end
+  else begin
+    if (!sel) begin
+      next_bits <= parallel_in;
+    end
+    else begin
+      next_bits <= xor_results;
+    end
+  end
+end
+
+// combinational part
+assign parallel_out = bits;
+assign bits = next_bits;
+
+genvar i;
+generate
+  for(i = 0;i < 6;i++) begin : g_shift
+    int prev_i = (i-1 + 6) % 6;
+    assign xor_results[i] = bits[i] ^ bits[prev_i];
+  end
+endgenerate
+
 endmodule
--- q2/LFSR_6bit_tb.sv
+++ q2/LFSR_6bit_tb.sv
@@ -1,5 +1,28 @@
 module LFSR_6bit_tb;
 
-// complete here
+logic clk = 0, rst_n = 1, sel = 1;
+logic [5:0] parallel_in, parallel_out;
+
+LFSR_6bit UUT(
+  .clk(clk), .rst_n(rst_n), .sel(sel),
+  .parallel_in(parallel_in), .parallel_out(parallel_out)
+  );
+
+always begin
+  #5 clk = 1;
+  #5 clk = 0;
+end
+
+initial begin
+  rst_n = 0;
+  #10;
+  rst_n = 1;
+  parallel_in = 6'b100101;
+  sel = 0;
+  #10;
+  sel = 1;
+  #20;
+end
 
 endmodule
+
