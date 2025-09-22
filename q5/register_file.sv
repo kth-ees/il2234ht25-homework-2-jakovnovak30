4441810cf912eecac4b84c946e05963f56ee0562
jakovnovak30 <jakovnovak30@gmail.com>
2025-09-22 17:12:09 +0200

    q5 done

--- q5/register_file.sv
+++ q5/register_file.sv
@@ -1,4 +1,4 @@
-module registerfile (input logic clk,
+module register_file (input logic clk,
                      input logic rst_n,
                      input logic write_en,
                      input logic [3:0] write_addr,
@@ -8,6 +8,28 @@ module registerfile (input logic clk,
                      output logic [7:0] data_out1,
                      output logic [7:0] data_out2
                      );
-    // complete here
+    localparam unsigned BW = 8, AW = 4;
+    localparam unsigned DEPTH = 2**AW;
+
+    logic [BW-1:0] registers [DEPTH];
+    logic [BW-1:0] out [2];
+
+    always_ff @(posedge clk or negedge rst_n) begin : seq_logic
+      if (!rst_n) begin
+        foreach (registers[i]) registers[i] <= '0;
+      end
+      else begin
+        out[0] <= registers[read_addr1];
+        out[1] <= registers[read_addr2];
+
+        if (write_en) begin
+          registers[write_addr] <= data_in;
+        end
+      end
+    end
+
+    assign data_out1 = out[0];
+    assign data_out2 = out[1];
 
 endmodule
+
--- q5/register_file_tb.sv
+++ q5/register_file_tb.sv
@@ -1,5 +1,49 @@
-module registerfile_tb;
+module register_file_tb;
 
 // complete here
+logic clk = 0, rst_n = 1, write_en = 0;
+logic [3:0] write_addr = '0, read_addr1 = '0, read_addr2 = '0;
+logic [7:0] data_in = '0, data_out1, data_out2;
+
+register_file UUT (clk,
+                   rst_n,
+                   write_en,
+                   write_addr,
+                   data_in,
+                   read_addr1,
+                   read_addr2,
+                   data_out1,
+                   data_out2);
+
+always begin
+  #5 clk = 1;
+  #5 clk = 0;
+end
+
+logic [7:0] registers [16];
+
+initial begin
+  rst_n = 0;
+  #10;
+  rst_n = 1;
+  write_en = 1;
+  for (int i=0;i < 100;i++) begin
+    automatic logic [3:0] adr = $urandom;
+    automatic logic [7:0] val = $urandom;
+    registers[adr] = val;
+    write_addr = adr; data_in = val;
+    #10;
+  end
+
+  write_en = 0;
+  for (int i=0; i < 16;i++) begin
+    read_addr1 = i;
+    read_addr2 = (i + 1) % 16;
+    #10;
+    assert (data_out1 == registers[read_addr1]);
+    assert (data_out2 == registers[read_addr2]);
+  end
+end
 
 endmodule
+
